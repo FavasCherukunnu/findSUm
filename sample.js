c1a636e1e34b5a8fe9fc54e35b7a72de3301e185
Favas Cherukunnu <mhdfavascheru@gmail.com>
2023-07-12 07:21:34 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int arr[] ={2,1,5,3,6,2,4};
+    int givenNum= 6;
+    int size=7;
+    int tempArr[size],sum=0,tempArrIndex=0;
+    
+    for(int i=0;i<size;i++){
+        sum = arr[i];
+        tempArrIndex=0;
+        tempArr[tempArrIndex++] = arr[i];
+        for(int j=0;j<size;j++){
+            
+            
+            if(i==j){
+                continue;
+            }
+            
+            //clearing temp aray
+            for(int temp=0;temp<size;temp++){
+                tempArr[temp]=0;
+            }
+            tempArrIndex=0;
+            //
+            sum = arr[i];
+            tempArr[tempArrIndex++] = arr[i];
+            sum += arr[j];
+            tempArr[tempArrIndex++] = arr[j];
+            
+            if(sum==givenNum){
+                for(int temp=0;temp<tempArrIndex;temp++){
+                    printf("%d,",tempArr[temp]);
+                }
+                printf("\n");
+                for(int temp=0;temp<size;temp++){
+                    tempArr[temp]=0;
+                }
+            }
+            
+        }
+        
+        
+    }
+
+    return 0;
+}
+
+
+ 
